48e2eca052986a09a33cd23c100626ae260853e9
jishnu <jishnu.dev.roy@gmail.com>
2024-02-19 14:43:36 +0530

    refactoring

--- models/cart.js
+++ models/cart.js
@@ -1,51 +1,46 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const p = path.join(
-    path.dirname(require.main.filename),
-    'data',
-    'cart.json'
-);
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
 
 module.exports = class Cart {
-    static addProduct(id, productPrice) {
-        // fetch preveious cart
+  static addProduct(id, productPrice) {
+    // fetch preveious cart
 
-        fs.readFile(p, (err, fileContent) => {
-            let cart = { products: [], totalPrice: 0 };
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
 
-            if (!err) {
-                cart = JSON.parse(fileContent);
-            }
-            const existingProductIndex = cart.products.findIndex(p => p.id === id);
-            const exsistingProduct = cart.products[existingProductIndex];
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      const existingProductIndex = cart.products.findIndex((p) => p.id === id);
+      const exsistingProduct = cart.products[existingProductIndex];
 
-            let updatedProduct;
-            if (exsistingProduct) {
-                updatedProduct = {...exsistingProduct};
-                updatedProduct.qty = updatedProduct.qty + 1;
-                cart.products[existingProductIndex] = updatedProduct;
-            } else {
-                updatedProduct = {id: id, qty: 1};
-                cart.products = [...cart.products, updatedProduct];
-            }
-            cart.totalPrice = parseInt(cart.totalPrice) + parseInt(productPrice);
-            fs.writeFile(p, JSON.stringify(cart), err => {
-                console.log(err);
-            });
-        })
-        // Analyze cart
-        // add / increment cart
+      let updatedProduct;
+      if (exsistingProduct) {
+        updatedProduct = { ...exsistingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = parseInt(cart.totalPrice) + parseInt(productPrice);
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+    // Analyze cart
+    // add / increment cart
+  }
 
-    }
-
-    static getAllCartItems(cb) {
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            } else {
-                cb(JSON.parse(fileContent));
-            }
-        });
-    }
-}
+  static getAllCartItems(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      } else {
+        cb(JSON.parse(fileContent));
+      }
+    });
+  }
+};
