f87ae0af19ff6ac6b82f51490b03b10e3425e093
jishnu <jishnu.dev.roy@gmail.com>
2024-02-21 22:26:25 +0530

    fix findAll methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,13 +15,15 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  
-  req.user.createProduct({
-    title: title,
-    imageUrl: imageUrl,
-    price: price,
-    description: description,
-  }).then((result) => {
+
+  req.user
+    .createProduct({
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      description: description,
+    })
+    .then((result) => {
       console.log("Saved successfully");
       res.redirect("/");
     })
@@ -29,7 +31,8 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  req.user.getProducts()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -43,8 +46,11 @@ exports.getProducts = (req, res, next) => {
 exports.editProduct = (req, res, next) => {
   const prodId = req.params.productId;
 
-  Product.findByPk(prodId)
+  // Product.findByPk(prodId)
+  req.user
+    .getProducts({ where: { id: prodId } })
     .then((product) => {
+      product = product[0];
       res.render("admin/edit-product", {
         product: product,
         pageTitle: "Edit",
