5115cc9e60235551a1052ba52a0def01e98fdf1d
jishnu <jishnu.dev.roy@gmail.com>
2024-02-22 17:21:11 +0530

    aiven connected

--- util/database.js
+++ util/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('test', 'root', 'jishnudev', {
-    host: 'localhost',
+const sequelize = new Sequelize('defaultdb', 'avnadmin', 'AVNS_fnUAbZIB-hXi9ESZMfm', {
+    host: 'mysql-33ac2037-jdrmart.a.aivencloud.com',
+    port: 11655,
     dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
   });
 
