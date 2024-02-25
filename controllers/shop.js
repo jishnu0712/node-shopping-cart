c4ebf76b04d6b35b994faa1aa19fade489cf9706
jishnu <jishnu.dev.roy@gmail.com>
2024-02-25 17:12:16 +0530

    fix post cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -58,11 +58,33 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
-  const productId = req.body.productId;
+  const prodId = req.body.productId;
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      let newQuantity = 1;
+      if (product) {
+        // ...
+      }
+      return Product.findByPk(prodId)
+        .then((product) => {
+          return fetchedCart.addProduct(product, {
+            through: { quantity: newQuantity },
+          });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 
-  Product.findById(productId, (product) => {
-    Cart.addProduct(productId, product.price);
-  });
   res.redirect("/cart");
 };
 
