9b15d4cbe9cc5ad723260584fb21efcd30868e35
jishnu <jishnu.dev.roy@gmail.com>
2024-03-03 22:40:21 +0530

    sessions basic login page setup

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const errorController = require("./controllers/error");
-
-const mongoose = require("mongoose");
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const User = require("./models/user");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("65e330e6ac27816fc3167d02")
-    .then((user) => {
+  User.findById('65e3dd4116a0b1e2f1560e99')
+    .then(user => {
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -37,18 +38,21 @@ mongoose
   .connect(
     "mongodb+srv://clumpiness:r1fbR7A327xczldH@cluster0.qcwuzp2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
-  .then((result) => {
-    User.findOne().then((user) => {
+  .then(result => {
+    User.findOne().then(user => {
       if (!user) {
-        const newUser = new User({
-          username: "Jishnu",
-          email: "clumpiness@gmail.com",
-          cart: { items: [] },
+        const user = new User({
+          name: 'Jishnu',
+          email: 'jishnu@test.com',
+          cart: {
+            items: []
+          }
         });
-        newUser.save();
+        user.save();
       }
     });
-
-    app.listen(3000, console.log("server started"));
+    app.listen(3000, console.log('server started'));
   })
-  .catch((err) => console.log(err));
+  .catch(err => {
+    console.log(err);
+  });
---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login'
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  // console.log(req.body.email);
+
+  res.redirect('/');
+};
