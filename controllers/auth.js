ebdd9aa96b1539b3ff3bf73f409f0689fb1f2139
jishnu <jishnu.dev.roy@gmail.com>
2024-03-13 09:18:32 +0530

    signup validations added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,37 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-const {sendOTPEmail} = require('../util/emailUtils');
-const User = require('../models/user');
+const { sendOTPEmail } = require("../util/emailUtils");
+const User = require("../models/user");
+
+const { validationResult } = require("express-validator");
 
 exports.getLogin = (req, res, next) => {
-  let message = req.flash('error');
+  let message = req.flash("error");
   if (message.length > 0) {
     message = message[0];
   } else {
     message = null;
   }
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    errorMessage: message
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
-  let message = req.flash('error');
+  let message = req.flash("error");
   if (message.length > 0) {
     message = message[0];
   } else {
     message = null;
   }
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup',
-    errorMessage: message
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMessage: message,
   });
 };
 
@@ -37,81 +39,86 @@ exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   User.findOne({ email: email })
-    .then(user => {
+    .then((user) => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login');
+        req.flash("error", "Invalid email or password.");
+        return res.redirect("/login");
       }
       bcrypt
         .compare(password, user.password)
-        .then(doMatch => {
+        .then((doMatch) => {
           if (doMatch) {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            return req.session.save(err => {
+            return req.session.save((err) => {
               console.log(err);
-              res.redirect('/');
+              res.redirect("/");
             });
           }
-          req.flash('error', 'Invalid email or password.');
-          res.redirect('/login');
+          req.flash("error", "Invalid email or password.");
+          res.redirect("/login");
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
-          res.redirect('/login');
+          res.redirect("/login");
         });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a different one.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          sendOTPEmail(["sadirul.islam786@gmail.com", "clumpiness@gmail.com"], 'Book O Pedia - Welcome', "Successfully signed up", cc=[], bcc=[] );
-          res.redirect('/login');
-        });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+  return bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
     })
-    .catch(err => {
-      console.log(err);
+    .then((result) => {
+      sendOTPEmail(
+        ["sadirul.islam786@gmail.com", "clumpiness@gmail.com"],
+        "Book O Pedia - Welcome",
+        "Successfully signed up",
+        (cc = []),
+        (bcc = [])
+      );
+      res.redirect("/login");
     });
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     console.log(err);
-    res.redirect('/');
+    res.redirect("/");
   });
 };
 
 exports.getReset = (req, res, next) => {
-  let message = req.flash('error');
+  let message = req.flash("error");
   if (message.length > 0) {
     message = message[0];
   } else {
     message = null;
   }
-  res.render('auth/reset', {
-    path: '/reset',
-    pageTitle: 'Reset Password',
-    errorMessage: message
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
   });
 };
 
@@ -119,53 +126,54 @@ exports.postReset = (req, res, next) => {
   crypto.randomBytes(32, (err, buffer) => {
     if (err) {
       console.log(err);
-      return res.redirect('/reset');
+      return res.redirect("/reset");
     }
-    const token = buffer.toString('hex');
+    const token = buffer.toString("hex");
     User.findOne({ email: req.body.email })
-      .then( user => {
+      .then((user) => {
         if (!user) {
-          req.flash('error', 'No account with that Email found');
-          return res.redirect('/reset');
+          req.flash("error", "No account with that Email found");
+          return res.redirect("/reset");
         }
         user.resetToken = token;
         user.resetTokenExpiration = Date.now() + 3600000;
         return user.save();
       })
-      .then(result => {
-        res.redirect('/');
-        sendOTPEmail([req.body.email],
-         'Book O Pedia - Reset Link', 
-         `<h1>You requested a password reset</h1>
+      .then((result) => {
+        res.redirect("/");
+        sendOTPEmail(
+          [req.body.email],
+          "Book O Pedia - Reset Link",
+          `<h1>You requested a password reset</h1>
           Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a password.
-         `, 
-         cc=[], bcc=[] );
-
+         `,
+          (cc = []),
+          (bcc = [])
+        );
       })
-      .catch(err => console.log(err));
-
-  })
+      .catch((err) => console.log(err));
+  });
 };
 
 exports.getNewPassword = (req, res, next) => {
   const token = req.params.token;
-  User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
-    .then(user => {
-      let message = req.flash('error');
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error");
       if (message.length > 0) {
         message = message[0];
       } else {
         message = null;
       }
-      res.render('auth/new-password', {
-        path: '/new-password',
-        pageTitle: 'New Password',
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
         errorMessage: message,
         userId: user._id.toString(),
         passwordToken: token,
       });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postNewPassword = (req, res, next) => {
@@ -177,26 +185,29 @@ exports.postNewPassword = (req, res, next) => {
   User.findOne({
     resetToken: passwordToken,
     resetTokenExpiration: { $gt: Date.now() },
-    _id: userId
+    _id: userId,
   })
-  .then(user => {
-    resetUser = user;
-    return bcrypt.hash(newPassword, 12);
-  })
-  .then(hashedPassword => {
-    resetUser.password = hashedPassword;
-    resetUser.resetToken = undefined;
-    resetUser.resetTokenExpiration = undefined;
-    return resetUser.save();
-  })
-  .then(result => {
-    res.redirect("/login");
-    sendOTPEmail([req.body.email],
-      'Book O Pedia - Reset Link', 
-      `<h1>Your password has been reset!</h1>
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+      sendOTPEmail(
+        [req.body.email],
+        "Book O Pedia - Reset Link",
+        `<h1>Your password has been reset!</h1>
        If not done by you, then revert back to this email.
-      `, 
-      cc=[], bcc=[] );
-  })
-  .catch(err => console.log(err));
+      `,
+        (cc = []),
+        (bcc = [])
+      );
+    })
+    .catch((err) => console.log(err));
 };
