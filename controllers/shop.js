f53c7d151411708a60920f1545697f78ebd90eeb
jishnu <jishnu.dev.roy@gmail.com>
2024-03-26 00:10:07 +0530

    stripe integrated

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,6 +3,7 @@ const Order = require("../models/order");
 const fs = require("fs");
 const path = require("path");
 const PDFDocument = require('pdfkit');
+const stripe = require('stripe')('sk_test_51OyGIcSHHq8FjHWPUAx2JGrCTsyt4c9fmPTCnczMZb9J2z0X95A8LNmNQKLGKjzNeQXmMO0gNUYTRS9Sta42kkAr00WDC9Vag6');
 
 const ITEMS_PER_PAGE = 1;
 
@@ -124,6 +125,85 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .removeFromCart(prodId)
     .then((result) => {
       res.redirect("/cart");
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+  let products;
+let total = 0;
+
+req.user
+  .populate("cart.items.productId")
+  .then((user) => {
+    products = user.cart.items;
+    
+    products.forEach(p => {
+      total += p.quantity * p.productId.price;
+    })
+
+    return stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: products.map(p => {
+        return {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: p.productId.title,
+              description: p.productId.description,
+            },
+            unit_amount: p.productId.price * 100, // Convert price to cents
+          },
+          quantity: p.quantity,
+        }
+      }),
+      mode: 'payment',
+      success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+      cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+    });
+  })
+    .then(session => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      console.log(err)
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
     })
     .catch((err) => {
       const error = new Error(err);
